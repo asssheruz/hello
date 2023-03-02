3a8e1c15691888dc21c020ecbc3c186d4d16cc72
Sherzod <asssher.uz@gmail.com>
2023-03-02 21:48:45 +0500

    Added Task15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -6,4 +6,39 @@
 // 7 -> да
 // 1 -> нет
 
-
+int Prompt(string message)
+{
+	Console.Write(message);
+	string value = Console.ReadLine();
+	int result = Convert.ToInt32(value);
+	return result;
+}
+bool IsWeekend(int weekDay)
+{
+	if (weekDay > 5)
+	{
+		return true;
+	}
+	return false;
+}
+bool ValidateWeekDay(int number)
+{
+	if (number > 0 && number <= 7)
+	{
+		return true;
+	}
+	Console.WriteLine("Это не день недели!");
+	return false; 
+}
+int weekDay = Prompt("Введите день недели > ");
+if (ValidateWeekDay(weekDay))
+{
+	if (IsWeekend(weekDay))
+	{
+		Console.WriteLine("Наконец-то выходной");
+	}
+	else
+	{
+		Console.WriteLine("Придется поработать");
+	}
+}
