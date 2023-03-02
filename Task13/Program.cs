f9575bc5b36c1b8fff2d049c934e9f0045dce590
Sherzod <asssher.uz@gmail.com>
2023-03-02 21:37:33 +0500

    Added Task13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,27 +1,37 @@
-﻿int number = new Random().Next(10, 100);
-Console.WriteLine($"Случайное число из отрезка 10 - 99 -> {number}");
-int firstDigit = number / 10;
-int secondDigit = number % 10;
+﻿// 13: Напишите программу, которая выводит
+//  третью цифру заданного числа или сообщает,
+//  что третьей цифры нет.
 
-// if (firstDigit > secondDigit)
-// {
-//     Console.WriteLine($"Наибольшая цифра числа -> {firstDigit}");
-// }
-// else
-// {
-//     Console.WriteLine($"Наибольшая цифра числа -> {secondDigit}");
-// }
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
 
-// int result = firstDigit > secondDigit ? firstDigit : secondDigit;
-// Console.WriteLine($"Наибольшая цифра числа -> {result}");
-
-int maxDigit = MaxDigit(number);
-Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
-
-int MaxDigit(int num) // num = number
+int Prompt(string message)
 {
-	int firstDigit = num / 10;
-	int secondDigit = num % 10;
-	int result = firstDigit > secondDigit ? firstDigit : secondDigit;
+	Console.Write(message);
+	string value = Console.ReadLine();
+	int result = Convert.ToInt32(value);
 	return result;
+}
+int GetTrirdRank(int number)
+{
+	while (number > 999)
+	{
+		number /= 10;
+	}
+	return number % 10;
+}
+bool ValidateNumber (int number)
+{
+	if (number < 100)
+	{
+		Console.WriteLine("Третьей цифры нет");
+		return false;
+	}
+	return true;
+}
+int number = Prompt("Введите число > ");
+if (ValidateNumber (number))
+{
+	Console.WriteLine(GetTrirdRank(number));
 }
