0222470165920da2a838fa4f9dde6daf9633c4ca
Sherzod <asssher.uz@gmail.com>
2023-03-06 20:05:42 +0500

    Added Task21

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -6,5 +6,25 @@
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
 
+Console.WriteLine("Введите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
 
+if (number > 0)
+{
+	TableSquare(number);
+}
+else
+{
+	Console.WriteLine("Введено некорректное число");
+}
+
+void TableSquare(int num)
+{
+	int i = 1;
+	while (i <= num)
+	{
+		Console.WriteLine($"{i,4} -> {i * i * i,4}");
+		i++;
+	}
+}
 
