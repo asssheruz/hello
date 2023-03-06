3f9763b8cbf64cd2e3fdb319640544d0af4b18cb
Sherzod <asssher.uz@gmail.com>
2023-03-07 00:20:37 +0500

    Added Task30

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -7,3 +7,15 @@
 // 12821 -> да
 // 23432 -> да
 
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0) number *= -1;
+if (number > 9999 && number < 100000)
+{
+	int result = number % 10;
+	Console.WriteLine(result);
+}
+else
+{
+	Console.WriteLine("Нужно ввести пятизначное число");
+}
