777999946eeb3e30ebded164bc0bf6ab40c91271
Sherzod <asssher.uz@gmail.com>
2023-03-06 20:22:12 +0500

    Added Task21

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -6,3 +6,25 @@
 // A (3,6,8); B(2, 1, -7), -> 15.84
 // A(7, -5, 0); B(1, -1, 9)-> 11.53
 
+Console.WriteLine("Введите координаты точки A");
+Console.Write("X: ");
+int xa = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int ya = Convert.ToInt32(Console.ReadLine());
+Console.Write("Z: ");
+int za = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки B");
+Console.Write("X: ");
+int xb = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int yb = Convert.ToInt32(Console.ReadLine());
+Console.Write("Z: ");
+int zb = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"A({xa}, {ya}, {za}); B({xb}, {yb}, {zb}) -> {Math.Round(Distance(xa, ya, za, xb, yb, zb), 2, MidpointRounding.ToZero)}");
+
+double Distance(int xa, int ya, int za, int xb, int yb, int zb)
+{
+	double dist = Math.Sqrt(Math.Pow(xb - xa, 2) + Math.Pow(yb - ya, 2) + Math.Pow(zb - za, 2));
+	return dist;
+}
